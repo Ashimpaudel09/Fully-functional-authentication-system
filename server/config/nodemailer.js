41b145e89153b177830e35276db7a549620c768c
Ashimpaudel09 <158179981+Ashimpaudel09@users.noreply.github.com>
2025-10-25 20:37:13 +0545

    s

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -11,13 +11,5 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Optional: verify transporter once (helps catch bad credentials)
-transporter.verify(function (error, success) {
-  if (error) {
-    console.error("SMTP Connection Error:", error.message);
-  } else {
-    console.log("SMTP Server is ready to send messages");
-  }
-});
 
 export default transporter;
